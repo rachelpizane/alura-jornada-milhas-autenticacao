4188c61ab09e3c3f4f626a975315aa53cf551b46
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-29 15:46:53 -0300

    feat: incluido a logica do formulario no login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formLogin!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.formLogin = this.formBuilder.group({
+      email: [''],
+      senha: ['']
+    });
+  }
+
+  login(): void {
+    console.log(this.formLogin.value['email']);
+    console.log(this.formLogin.value['senha']);
+  }
 }
